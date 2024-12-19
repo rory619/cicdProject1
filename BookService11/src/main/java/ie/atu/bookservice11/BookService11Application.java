19b72ffed7892b6e0e43fbe75becb7d7c41f1608
rory619 <g00422319@atu.ie>
2024-12-19 15:42:22 +0000

    Add BookService11, CustomerService, and OrderService microservices

---
+++ BookService11/src/main/java/ie/atu/bookservice11/BookService11Application.java
@@ -0,0 +1,13 @@
+package ie.atu.bookservice11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookService11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookService11Application.class, args);
+    }
+
+}
